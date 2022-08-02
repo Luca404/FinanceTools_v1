2fb3d1e1c00777002b8d34bd6a4326e3bceff30a
Luca Botta <luca.botta44@gmail.com>
2022-08-02 14:11:45 +0200

    Create a separate login js file

--- public/js/scriptPfManager.js
+++ public/js/scriptPfManager.js
@@ -1,7 +1,10 @@
+import * as login from './manageLogin.js';
+
 //Connection to server
 const server = io();
 server.on("connect", () => {
     console.log("Connected");
+	login.showLoginModal();
 });
  
 server.on("disconnect", () => {
@@ -10,9 +13,9 @@ server.on("disconnect", () => {
 
 server.on("loginResult", (data) => {
 	if( data["status"] )
-		successLogin( data["text"] );
+		login.successLogin( data["text"] );
 	else
-		failedLogin( data["text"] );
+		login.failedLogin( data["text"] );
 });
 
 server.on("returnPfList", (data) => {
@@ -28,7 +31,16 @@ server.on("returnTickersList", (data) => {
 	showTickersInInput(data);
 });
 
-//Constant
+//Add onload function to body
+document.getElementById("corpo").addEventListener("load", setEvent(), false);
+
+//Set event
+function setEvent(){
+	$("#registerButton").click( login.showRegisterModal );
+	$("#loginButton").click( login.checkLogin );
+	$("#registerButton2").click( login.registerUser );
+	$("#loginButton2").click( login.showLoginModal );
+}
 
 var multipleSelect = new Choices('#addPfTickersInput', {
 	removeItemButton: true,
@@ -283,192 +295,3 @@ function addPfButton(){
 		}, 1500);
 	}
 }
-
-//Show modal for login
-function showLoginModal(){
-	let usern = checkIfLogged()
-	if( usern == "" ){
-		$("#content").css("filter", "blur(5px)");
-		$('#loginModal').modal({backdrop: 'static', keyboard: false, show: true});
-		$("#registrationModal").modal("hide");
-		$('#registrationModal').data('bs.modal',null);
-	}
-	else{
-		drawProfileDiv(usern);
-	}
-}
-
-//Function to check if still logged
-function checkIfLogged(){
-	let decodedCookie = decodeURIComponent(document.cookie);
-	let cookie = decodedCookie.split(';');
-	let cookieName = "username=";
-	for(let i = 0; i <cookie.length; i++) {
-		let c = cookie[i];
-		while (c.charAt(0) == ' ') {
-		  c = c.substring(1);
-		}
-		if (c.indexOf(cookieName) == 0) {
-		  return c.substring(cookieName.length, c.length);
-		}
-	}
-	return "";
-}
-
-
-//Show modal for registration
-function showRegisterModal(){
-	$('#registrationModal').modal({backdrop: 'static', keyboard: false, show: true});
-	$("#loginModal").modal("hide");	
-	$('#loginModal').data('bs.modal',null);
-}
-
-//Call server and check if login is ok
-function checkLogin(){
-	var usrn = document.getElementById("usernInput");
-	var passwd = document.getElementById("passwdInput");
-	if(usrn.value == ""){
-		usrn.style.animation = "0.25s linear 0s 1 normal forwards running error";
-		usrn.placeholder = "Insert username";
-		setTimeout(() => {
-			usrn.style.animation = "";
-		}, 250);
-		setTimeout(() => {
-			usrn.placeholder = "";
-		}, 1500);
-	}
-	if(passwd.value == ""){
-		passwd.style.animation = "0.25s linear 0s 1 normal forwards running error";
-		passwd.placeholder = "Insert password";
-		setTimeout(() => {
-			passwd.style.animation = ""
-		}, 250);
-		setTimeout(() => {
-			passwd.placeholder = "";
-		}, 1500);
-	}
-	
-	if( usrn.value != "" && passwd.value != "" ){		
-		var hashPass = hash(passwd.value);
-		server.emit("login",{"username":usrn.value, "password":hashPass});
-	}
-}
-
-//If login is successful
-function successLogin( usern ){
-	var loginModal = document.getElementById("loginModal");
-	var content = loginModal.getElementsByClassName("modal-content")[0];
-	content.style = "filter: blur(10px)";
-	var divSuccess = document.createElement("div");
-	divSuccess.appendChild(document.createTextNode("Successfully logged in!"));
-	divSuccess.id = "successLoginDiv";
-	loginModal.appendChild(divSuccess);
-	content.style.pointerEvents = "none";
-	loginModal.style.userSelect = "none";
-	setTimeout(() => {
-		$("#loginModal").modal("hide");
-		$("#content").css("filter", "");
-	}, 2000);
-	setCookie("username",usern,5);
-	drawProfileDiv(usern);
-}
-
-//Save cookies function
-function setCookie(cname, cvalue, exdays) {
-	const d = new Date();
-	d.setTime(d.getTime() + (exdays*24*60*60*1000));
-	let expires = "expires="+ d.toUTCString();
-	document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
-}
-
-//If login is unsuccessful
-function failedLogin( error ){
-	var usrn = document.getElementById("usernInput");
-	var passwd = document.getElementById("passwdInput");
-	if( error == "Wrong Password" ){
-		passwd.value = "";
-		passwd.style.animation = "0.25s linear 0s 1 normal forwards running error";
-		passwd.placeholder = "Wrong password";
-		setTimeout(() => {
-			passwd.style.animation = ""
-		}, 250);
-		setTimeout(() => {
-			passwd.placeholder = "";
-		}, 2000);
-	}
-	else{
-		usrn.value = "";
-		passwd.value = "";
-		usrn.style.animation = "0.25s linear 0s 1 normal forwards running error";
-		usrn.placeholder = "Account doesnt exist";
-		setTimeout(() => {
-			usrn.style.animation = "";
-		}, 250);
-		setTimeout(() => {
-			usrn.placeholder = "";
-		}, 2000);
-	}
-}
-
-//Draw profile div
-function drawProfileDiv( username ){
-	let profileDiv = document.getElementById("profileDiv");
-	let profileP = profileDiv.getElementsByTagName("p")[0];
-	profileP.innerText = "User:    " + username;
-	server.emit("getPfList",{"username":username});
-}
-
-//function for register a user
-function registerUser(){
-	var usrn = document.getElementById("usernInputReg");
-	var passwd = document.getElementById("passwdInputReg");
-	var passwdConf = document.getElementById("passwdInputConf");
-	if(usrn.value == ""){
-		usrn.style.animation = "0.25s linear 0s 1 normal forwards running error";
-		usrn.placeholder = "Insert username";
-		setTimeout(() => {
-			usrn.style.animation = "";
-		}, 250);
-		setTimeout(() => {
-			usrn.placeholder = "";
-		}, 1500);
-	}
-	if(passwd.value == ""){
-		passwd.style.animation = "0.25s linear 0s 1 normal forwards running error";
-		passwd.placeholder = "Insert password";
-		setTimeout(() => {
-			passwd.style.animation = ""
-		}, 250);
-		setTimeout(() => {
-			passwd.placeholder = "";
-		}, 1500);
-	}
-	if(passwdConf.value == ""){
-		passwdConf.style.animation = "0.25s linear 0s 1 normal forwards running error";
-		passwdConf.placeholder = "Confirm password";
-		setTimeout(() => {
-			passwdConf.style.animation = ""
-		}, 250);
-		setTimeout(() => {
-			passwdConf.placeholder = "";
-		}, 1500);
-	}
-	if(passwd.value != passwdConf.value){
-		passwdConf.style.animation = "0.25s linear 0s 1 normal forwards running error";
-		passwdConf.value = "";
-		passwdConf.placeholder = "Passwords dont match";
-		setTimeout(() => {
-			passwdConf.style.animation = ""
-		}, 250);
-		setTimeout(() => {
-			passwdConf.placeholder = "";
-		}, 1500);
-	}
-
-	if( usrn.value != "" && passwd.value != "" && passwd.value == passwdConf.value && false ){		
-		var hashPass = hash(passwd.value);
-		server.emit("register",{"username":usrn.value, "password":hashPass});
-	}
-}
-
-function hash(e){for(var r=0,i=0;i<e.length;i++)r=(r<<5)-r+e.charCodeAt(i),r&=r;return r};
